af4f907ae8afc8d21c3637884c91a8026b238267
rahultiwari56 <rahultiwari56@gmail.com>
2022-05-27 05:07:17 +0530

    working on django query

--- library/urls.py
+++ library/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.index),
     path('search-student/', views.find_student, name='search-student'),
     path('add-student/', views.add_student, name='add-student'),
-    path('get-student/<int:pk>/', views.get_student, name='get-student')
+    path('get-student/<id>/', views.get_student, name='get-student')
 ]
--- library/views.py
+++ library/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, HttpResponse
+from library.models import Student, School, Book
 
 def index(request):
     return render(request, 'index.html')
@@ -11,4 +12,14 @@ def add_student(request):
     return render(request, 'add_student.html')
 
 def get_student(request, id):
-    pass
+    # print(id)
+    # student_details = Student.objects.filter(id=id)
+
+    student_details = Student.objects.filter(id=id).values('id', 'first_name', 'last_name', 'email', 'gender', 'school', 'book')
+
+    # print(student_details)
+    # if student_details:
+    #     details = student_details[0]
+    #     print(details)
+  
+    return HttpResponse(student_details)
